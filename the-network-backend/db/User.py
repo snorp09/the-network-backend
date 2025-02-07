76783f19816068629a7abbb6bb608f4f40be744f
Christopher Waschke <chrisdwaschke@gmail.com>
2025-02-07 13:16:23 -0800

    Moved User into Models package.

--- the-network-backend/db/User.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from .core import Base, SessionLocal
-
-class User(Base):
-    __tablename__ = "users"
-    username: Mapped[str] = mapped_column("username", str)
-    email: Mapped[str] = mapped_column("email", str)
-    password: Mapped[str] = mapped_column("password", str)
---
+++ the-network-backend/db/models/User.py
@@ -0,0 +1,14 @@
+from typing import TYPE_CHECKING, List
+from sqlalchemy.orm import Mapped, mapped_column
+from .core import Base
+
+if(TYPE_CHECKING):
+    from .Post import Post
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column()
+    email: Mapped[str] = mapped_column()
+    password: Mapped[str] = mapped_column()
+    posts: Mapped[List["Post"]] = mapped_column(back_populates="user")
