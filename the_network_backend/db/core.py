94547901f2d4809971ac9cee02f36e96be13d45c
Christopher Waschke <chrisdwaschke@gmail.com>
2025-03-31 16:51:40 -0700

    Added dotenv for config dict. Exported URL for importation into alembic.

--- the_network_backend/db/core.py
+++ the_network_backend/db/core.py
@@ -7,6 +7,10 @@ from sqlalchemy import create_engine, URL
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from os import environ
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Dictionary to hold database configuration
 config_dict = {
@@ -14,19 +18,21 @@ config_dict = {
     "DB_USER": environ.get("DB_USER"),
     "DB_PASSWORD": environ.get("DB_PASSWORD"),
     "DB_HOST": environ.get("DB_HOST"),
-    "DB_PORT": environ.get("DB_PORT")
+    "DB_PORT": environ.get("DB_PORT"),
+    "DB_NAME": environ.get("DB_NAME")
 }
 
-# Create our database engine. Using the the config_dict above to fill in the values
-engine = create_async_engine(
-    URL.create(
-        drivername=config_dict["DB_DRIVER"],
-        username=config_dict["DB_USER"],
-        password=config_dict["DB_PASSWORD"],
-        host=config_dict["DB_HOST"],
-        port=config_dict["DB_PORT"],
-    )
+DB_URL = URL.create(
+    drivername=config_dict["DB_DRIVER"],
+    username=config_dict["DB_USER"],
+    password=config_dict["DB_PASSWORD"],
+    host=config_dict["DB_HOST"],
+    port=config_dict["DB_PORT"],
+    database=environ.get("DB_NAME")
 )
+
+# Create our database engine. Using the the config_dict above to fill in the values
+engine = create_async_engine(url=DB_URL)
 
 # Create an async session maker
 SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
